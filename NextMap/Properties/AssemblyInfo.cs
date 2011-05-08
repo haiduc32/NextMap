ca9d24ce242cad3d4fc817c1fe5e97a60c4379e3
Radu Pascal <haiduc32@gmail.com>
2011-05-08 12:02:22 +0300

    added compiled assembly to the repo

--- NextMap/Properties/AssemblyInfo.cs
+++ NextMap/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NextMap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Object mapping by convention")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NextMap")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.01.0.0")]
+[assembly: AssemblyFileVersion("0.01.0.0")]
